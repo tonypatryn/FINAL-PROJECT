d08d36b791cc8d07fb8e045cdcfecec7e8101383
tonypatryn <tony.patryn@marketsgroup.org>
2016-11-02 00:06:15 -0400

    Major Update

--- jquery test/js/javascript.js
+++ jquery test/js/javascript.js
@@ -1,38 +1,23 @@
-var storyInputOne;
-var storyInputTwo;
-var storyInputThree;
-var fullStory;
+var corpseNumber = 3;
+var currentCorpse = 1;
+var currentInput = "";
+var fullStory = "";
+var getLastThree = "";
+var setLastThree = "";
 
-
-$('.buttonOne').on('click', function( oneClick ){
+$('.button').on('click', function( oneClick ) {
 	oneClick.preventDefault();
-	storyInputOne = $('#inputOne').val();
-	var inputOneLast3;
-	inputOneLast3 = storyInputOne.split(" ");
-	$("#inputOne").prop('disabled', true);
-	$('#inputOne').val( (inputOneLast3[inputOneLast3.length - 3]) + " " + (inputOneLast3[inputOneLast3.length - 2]) + " " + (inputOneLast3[inputOneLast3.length - 1]) );
+	if ( currentCorpse !== corpseNumber ) {
+		fullStory = fullStory + " " + $('#inputStory').val();
+		getLastThree = fullStory.split(" ");
+		setLastThree = (getLastThree[getLastThree.length - 3]) + " " + (getLastThree[getLastThree.length - 2]) + " " + (getLastThree[getLastThree.length - 1]);
+		$('.theStory').text(setLastThree);
+	} else {
+		fullStory = fullStory + " " + $('#inputStory').val();
+		$('.theStory').text(fullStory);
+		$('#inputStory').css('display','none');
+		$('.button').css('display','none');
+	}
+	$('#inputStory').val('');
+	currentCorpse++
 });
-
-$('.buttonTwo').on('click', function( TwoClick ){
-	TwoClick.preventDefault();
-	storyInputTwo = $('#inputTwo').val();
-	var inputTwoLast3;
-	inputTwoLast3 = storyInputTwo.split(" ");
-	$("#inputTwo").prop('disabled', true);
-	$('#inputTwo').val( (inputTwoLast3[inputTwoLast3.length - 3]) + " " + (inputTwoLast3[inputTwoLast3.length - 2]) + " " + (inputTwoLast3[inputTwoLast3.length - 1]) );
-});
-
-$('.buttonThree').on('click', function( threeClick ){
-	threeClick.preventDefault();
-	storyInputThree = $('#inputThree').val();
-	var inputThreeLast3;
-	inputThreeLast3 = storyInputThree.split(" ");
-	$("#inputThree").prop('disabled', true);
-	$('#inputThree').val( (inputThreeLast3[inputThreeLast3.length - 3]) + " " + (inputThreeLast3[inputThreeLast3.length - 2]) + " " + (inputThreeLast3[inputThreeLast3.length - 1]) );
-
-	fullStory = storyInputOne + " " + storyInputTwo + " " + storyInputThree;
-	$('.theStory').text(fullStory);
-});
-
-
-
--- js/javascript.js
+++ js/javascript.js
@@ -1,80 +1,35 @@
-var corpseTitle;
-var corpseNumber;
+var corpseTitle = "";
+var corpseNumber = 0;
+var currentCorpse = 1;
+var currentInput = "";
+var fullStory = "";
+var getLastThree = "";
+var setLastThree = "";
 
-// CREATE BUTTONS HOVER
-$('.inkBlotButton3').on({
-    'mouseover' : function() {
-    	$(this).attr('src','images/pick3-orange.png');
-    },
-    mouseout : function() {
-  		$(this).attr('src','images/pick3-black.png');
-    },
-});
-
-$('.inkBlotButton4').on({
-    'mouseover' : function() {
-    	$(this).attr('src','images/pick4-orange.png');
-    },
-    mouseout : function() {
-  		$(this).attr('src','images/pick4-black.png');
-    }
-});
-
-$('.inkBlotButton5').on({
-    'mouseover' : function() {
-    	$(this).attr('src','images/pick5-orange.png');
-    },
-    mouseout : function() {
-  		$(this).attr('src','images/pick5-black.png');
-    }
-});
-
-$('.inkBlotButton3').on('click', function(){
-    $(this).attr('src','images/pick3-orange.png');
-    $('.inkBlotButton4').attr('src','images/pick4-grey.png');
-    $('.inkBlotButton5').attr('src','images/pick5-grey.png');
-    corpseNumber = 3;
-});
-
-$('.inkBlotButton4').on('click', function(){
-    $(this).attr('src','images/pick4-orange.png');
-    $('.inkBlotButton3').attr('src','images/pick3-grey.png');
-    $('.inkBlotButton5').attr('src','images/pick5-grey.png');
-    corpseNumber = 4;
-});
-
-$('.inkBlotButton5').on('click', function(){
-    $(this).attr('src','images/pick5-orange.png');
-    $('.inkBlotButton3').attr('src','images/pick3-grey.png');
-    $('.inkBlotButton4').attr('src','images/pick4-grey.png');
-    corpseNumber = 5;
-});
 
 $('#letsPlay').on('click', function( playClick ) {
-	playClick.preventDefault();
-		$('.inputCorpseBoxes').html('');
-	for (var inputNumber = 0; inputNumber < corpseNumber; inputNumber++) {
-		$('.inputCorpseBoxes').append('<center><input type="text" name="nameHere" placeholder="Type Your Line"><div class="submitButton"><a href="#">Submit</a></div></center>');
-	}
+    corpseNumber = $('.corpse-number').val();
+    console.log(corpseNumber);
+    $('.inputCorpseBoxes').css('display','initial');
+    $('#howDoesSection').css('display','none');
+    $('.titleOfStory').css('display','none');
+    corpseTitle = $('.corpse-title').val();
+    $('.theStoryTitle').text(corpseTitle);
 });
 
-$('.submitButton').on('click', function( submitClick ) {
-	submitClick.preventDefault();
-	// var corpseCopyInput1;
-	// var corpseCopyInput2;
-	// var corpseCopyInput3;
-	// var corpseCopyInput4;
-	// var corpseCopyInput5;
-	// corpseCopyInput1 = $('input:nth-child(1)','value');
-	// corpseCopyInput2 = $('input:nth-child(2)','value');
-	// corpseCopyInput3 = $('input:nth-child(3)','value');
-	// corpseCopyInput4 = $('input:nth-child(4)','value');
-	// corpseCopyInput5 = $('input:nth-child(5)','value');
-	console.log('i think this is working?');
+$('.submitButton').on('click', function( oneClick ) {
+    oneClick.preventDefault();
+    if ( currentCorpse !== corpseNumber ) {
+        fullStory = fullStory + " " + $('#inputStory').val();
+        getLastThree = fullStory.split(" ");
+        setLastThree = (getLastThree[getLastThree.length - 3]) + " " + (getLastThree[getLastThree.length - 2]) + " " + (getLastThree[getLastThree.length - 1]);
+        $('.theStory').text(setLastThree);
+    } else {
+        fullStory = fullStory + " " + $('#inputStory').val();
+        $('.theStory').text(fullStory);
+        $('#inputStory').css('display','none');
+        $('.submitButton').css('display','none');
+    }
+    $('#inputStory').val('');
+    currentCorpse++
 });
-
-
-
-
-
-
