9a169f296c2b274936a366fbeee4a10679001ed7
tonypatryn <tony.patryn@marketsgroup.org>
2016-11-10 18:36:45 -0500

    90% final commit

--- js/javascript.js
+++ js/javascript.js
@@ -5,7 +5,9 @@ var currentInput = "";
 var fullStory = "";
 var getLastThree = "";
 var setLastThree = "";
+var navStatus = false;
 
+//CREATE THE CORPSE
 $('#letsPlay').on('click', function( playClick ) {
     corpseNumber = $('.corpse-number').val();
     $('.inputCorpseBoxes').css('display','block');
@@ -15,6 +17,7 @@ $('#letsPlay').on('click', function( playClick ) {
     $('.theStoryTitle').text(corpseTitle);
 });
 
+//SUBMIT THE CORPSE
 $('.submitButton').on('click', function( oneClick ) {
     oneClick.preventDefault();
     if ( currentCorpse < corpseNumber ) {
@@ -24,7 +27,10 @@ $('.submitButton').on('click', function( oneClick ) {
         $('.theStory').text(setLastThree);
     } else {
         fullStory = fullStory + " " + $('.inputStory').val();
-        $('.theStory').text(fullStory).hide().fadeIn(1000).css('padding-bottom','20px');
+        $('.theStory').text(fullStory).hide().fadeIn(1000).css({
+            'padding-bottom':'20px',
+            'height': '400px'
+        });
         $('.inputStory').css('display','none');
         $('.submitButton').css('display','none');
     }
@@ -32,12 +38,31 @@ $('.submitButton').on('click', function( oneClick ) {
     currentCorpse++
 });
 
+//ABOUT PAGE
 $('.aboutImage').hide().fadeIn(1000);
-
 $('.aboutText').hide().delay(400).fadeIn(1000);
 
+//HAMBURGER MENU
+$('.hamburger').on('click', function() {
+    if (navStatus === false) {
+        $('header').animate({ 
+            height:'255px'
+        }, 1000, function() {
+        navStatus = true;
+      });
+    } else {
+        $('header').animate({ 
+            height:'90px'
+        }, 1000, function() {
+        navStatus = false;
+      });
+    }
+});
 
-
+$(window).on('resize', function() {
+    $('header').css('height','90px');
+    navStatus = false;
+});
 
 
 
