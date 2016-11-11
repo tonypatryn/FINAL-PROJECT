b57699ab870e7ece629f2bc367c162089e9ef962
tonypatryn <tony.patryn@marketsgroup.org>
2016-11-10 21:47:23 -0500

    another commit

--- js/javascript.js
+++ js/javascript.js
@@ -27,12 +27,14 @@ $('.submitButton').on('click', function( oneClick ) {
         $('.theStory').text(setLastThree);
     } else {
         fullStory = fullStory + " " + $('.inputStory').val();
-        $('.theStory').text(fullStory).hide().fadeIn(1000).css({
+        $('.paper').addClass('paperOn');
+        $('.theStory').html('<p>' + fullStory + '</p>').hide().fadeIn(1000).css({
             'padding-bottom':'20px',
             'height': '400px'
         });
         $('.inputStory').css('display','none');
         $('.submitButton').css('display','none');
+        $('.theStory').append('<div id="letsPlay"><a href="create.html">Play Again?</a></div>');
     }
     $('.inputStory').val('');
     currentCorpse++
@@ -62,6 +64,15 @@ $('.hamburger').on('click', function() {
 $(window).on('resize', function() {
     $('header').css('height','90px');
     navStatus = false;
+});
+
+$('.theMap').addClass('scrolloff'); // set the pointer events to none on doc ready
+$('#gMaps').on('click', function () {
+    $('.theMap').removeClass('scrolloff'); // set the pointer events true on click
+});
+        // you want to disable pointer events when the mouse leave the canvas area;
+$(".theMap").mouseleave(function () {
+    $('.theMap').addClass('scrolloff'); // set the pointer events to none when mouse leaves the map area
 });
 
 
